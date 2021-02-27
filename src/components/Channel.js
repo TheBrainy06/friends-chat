ac9d95543fb0b49356b8cdc30bca1ab9b1137465
TheBrainy06 <gautamnsankar214@gmail.com>
2021-02-27 18:16:39 +0800

    Init Again

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -45,7 +45,7 @@ const Channel = ({ user = null }) => {
       // Clear input field
       setNewMessage('');
       // Scroll down to the bottom of the list
-      bottomListRef.current.scrollIntoView({ behavior: 'smooth' });
+      bottomListRef.current.scrollIntoView({ behavior: 'instant' });
     }
   };
 
